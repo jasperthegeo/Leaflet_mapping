4079e01d3548045f95711df6463953c799f6a5e3
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-02-20 14:30:01 -0500

    BLARGH

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,10 @@
-// Create map object centered around SF Area
+// Create a map object
 var myMap = L.map("map", {
-    center: [37.75, -122.48],
-    zoom: 8
-  });
+  center: [37.75, -122.48],
+  zoom: 8
+});
 
-//USGS GJSON URL
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
-
-  //Specified Tile Layer
+// Add a tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -17,13 +14,16 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+//Store url
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
-//Ingest data vai D3
+
+// Grab the data with d3
 d3.json(url, function(data) {
   function styleInfo(feature) {
     return {
       opacity: 1,
-      fillOpacity: 1,
+      fillOpacity: 0.5,
       fillColor: getColor(feature.properties.mag),
       color: "#000000",
       radius: getRadius(feature.properties.mag),
@@ -31,48 +31,73 @@ d3.json(url, function(data) {
       weight: 0.5
     };
   }
-  // Colour ramp 
-  function getColor(magnitude) {
+  // set different color from magnitude
+    function getColor(magnitude) {
     switch (true) {
     case magnitude > 5:
-      return "#ea2c2c";
+      return "#b81c1c";
     case magnitude > 4:
-      return "#b81c1c"; // Dark Red
+      return "#ea822c";
     case magnitude > 3:
-      return "#cf811b"; // Dark orange
+      return "#ee9c00";
     case magnitude > 2:
-      return "#d9ca29"; // orange-yellow
+      return "#79bf4b";
     case magnitude > 1:
-      return "#1cabb8"; //dark green
-    default:
-      return "#98ee00";
+      return "#2f7003";
+    default:  // Less than one
+      return "#1cabb8";
     }
   }
-
-    // set radius from magnitude
+  // set radius from magnitude
     function getRadius(magnitude) {
-      if (magnitude === 0) {
-        return 0;
-      }   
-    return Math.sqrt((magnitude+1)**3 * 2.718);
-  }
-
-   // GeoJSON layer
-   L.geoJson(data, {
-    // Make cricles
-    pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng);
-    },
-    // circle style
-    style: styleInfo,
-    // popup for each marker
-    onEachFeature: function(feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    if (magnitude <= 0) {
+      return 0;
     }
-  }).addTo(myMap);
 
-  // an object legend
-  var legend = L.control({
-    position: "topleft"
+    return Math.sqrt((magnitude+1)**2 * 2.718);
+  }
+    // GeoJSON layer
+    L.geoJson(data, {
+      // Make cricles
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng);
+      },
+      // circle style
+      style: styleInfo,
+      // popup for each marker
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+      }
+    }).addTo(myMap);
+  
+    // an object legend
+    var legend = L.control({
+      position: "topleft"
+    });
+  
+    // details for the legend
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+  
+      var colourRamp = [0, 1, 2, 3, 4, 5];
+      var colourList = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+      ];
+  
+      // Looping through
+      for (var i = 0; i < colourRamp.length; i++) {
+        div.innerHTML +=
+          "<i style='background: " + colourList[i] + "'></i> " +
+          colourRamp[i] + (colourRamp[i + 1] ? "&ndash;" + colourRamp[i + 1] + "<br>" : "+");
+      }
+      return div;
+    };
+  
+    // Finally, we our legend to the map.
+    legend.addTo(myMap);
   });
-      
